cfe5e523c00cfec73064af28748bd86ba0cf3e6c
Brandon Forsythe <bfforsythe@alaska.edu>
2021-01-14 15:42:21 -0900

    Added HW0A1

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+	std::cout << "Stopping by Woods on a Snowy Evening - Robert Frost" << "\n" << "------------------------------------" << "\n";
+	std::cout << "The woods are lovely, dark and deep," << "\n";
+	std::cout << "But I have promises to keep," << "\n";
+	std::cout << "And miles to go before I sleep," << "\n";
+	std::cout << "And miles to go before I sleep." << "\n";
+}
