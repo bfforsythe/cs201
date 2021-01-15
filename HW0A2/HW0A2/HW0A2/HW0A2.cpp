46cd0e8ae2c144b7ff6e2addc0d7d72a3039a826
Brandon Forsythe <bfforsythe@alaska.edu>
2021-01-15 09:44:40 -0900

    Added and Programmend HW0A2

---
+++ HW0A2/HW0A2/HW0A2/HW0A2.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+
+int main() {
+	std::cout << "In a world, where one mustachioed man ruins the lives" << "\n";
+	std::cout << "and -Bonds-" << "\n";
+	std::cout << "of four life long friends." << "\n" << "\n";
+	std::cout << "Follow them as they endlessly run around the board" << "\n";
+	std::cout << "Stealing properties, being incarcerated over and over again" << "\n";
+	std::cout << "Who will become the biggest bread winner in:" << "\n";
+	std::cout << "MONOPOLY" << "\n";
+}
