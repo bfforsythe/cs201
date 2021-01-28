d9ca333131ada3939c1eeba79d2f7997f2b23f89
Brandon Forsythe <business.brandonforsythe@gmail.com>
2021-01-27 17:33:07 -0900

    Created and coded requirements for CS201 HW01

---
+++ HW1/HW1/diamond.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main()
+{
+
+    using std::cout;
+    using std::cin;
+    using std::endl;
+
+    int dimOne, dimTwo, dimIn;
+    cout << "please enter a whole number value ";
+    cin >> dimIn;
+    for (dimOne = 0; dimOne <= dimIn; dimOne++)
+    {
+        for (dimTwo = 1; dimTwo <= dimIn - dimOne; dimTwo++) {
+            cout << " ";
+        }
+        for (dimTwo = 1; dimTwo <= 2 * dimOne - 1; dimTwo++) {
+            cout << "#";
+        }
+        cout << endl;
+    }
+    for (dimOne = dimIn - 1; dimOne >= 1; dimOne--)
+    {
+        for (dimTwo = 1; dimTwo <= dimIn - dimOne; dimTwo++) {
+            cout << " ";
+        }
+        for (dimTwo = 1; dimTwo <= 2 * dimOne - 1; dimTwo++) {
+            cout << "#";
+        }
+        cout << endl;
+    }
+}
