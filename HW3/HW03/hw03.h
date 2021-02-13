a9953329cb41ac3fa23845db696e33e85536e33e
Brandon Forsythe <bfforsythe@alaska.edu>
2021-02-12 21:31:58 -0900

    "finished" hw03

--- HW3/HW03/hw03.h
+++ HW3/HW03/hw03.h
@@ -2,28 +2,39 @@
 
 #include <iostream>
 #include <string>
+#include "hw03.h"
 
 using namespace std;
-
 int i;
+int k = 0;
 
 int box(int boxSize, string boxWords) {
 
-	for ( i = 0; i < 10 + boxSize; i++) {
+	for ( i = 0; i <= 10 + boxSize; i++) {
 		cout << "*";
 		}
-	for (int j = 0; j < 12; j++) {
-		if (j == 1 || j == 12) {
+	cout << endl;
+
+	for (int j = 0; j <= i; j++) {
+
+
+		if (j == 0 || j == i -1) {
 			cout << "*";
+			while (k != 1) {
+				cout << boxWords;
+				k++;
+			}
 		}
 		else {
+
 			cout << " ";
 		}
-		cout << endl;
 	}
 
+	cout << endl;
 
-	for (int i = 0; i < 10 + boxSize; i++) {
+
+	for (int i = 0; i <= 10 + boxSize; i++) {
 		cout << "*";
 	}
 
