0bfb23b037f1ce5efef6bb1513e668ae8197552a
Brandon Forsythe <business.brandonforsythe@gmail.com>
2021-02-21 19:44:23 -0900

    too tired to continue work. Working on AnalyzeTokens(). Program Crashes.

--- HW04/HW04/tokenizer.h
+++ HW04/HW04/tokenizer.h
@@ -6,10 +6,38 @@
 using namespace std;
 string str;
 string i;
+int j = 0;
+char a = 60;
+char z = 96;
+
+
+// Functionality for Sorting
+
+
+void AnalyzeTokens() {
+
+	vector<string> token;
+	for (int k = 0; k <= token.size(); k++){
+		if (token.at(j) != "a" && token.at(j) != "z" ) {
+			cout << "[integer]";
+			k++;
+		}
+		else if (token.at(j) != "1","2","3","4","5","6","7","8","9","0") {
+			cout << "[string]";
+			k++;
+		}
+		else if (token.at(j) == " ") {
+			cout << "[whitespace]";
+		}
+		else {
+			cout << "[unknown]";
+			k++;
+		}
+	}
+}
 
 void parse() {
 	unsigned StringtoToken(const string & input, vector<string>&tokens);
-	int j = 0;
 	vector<string> token;
 	istringstream input(str);
 
@@ -17,11 +45,11 @@ void parse() {
 		input >> i;
 		token.push_back(i);
 		if (j < token.size() - 1) {
-			cout << token.at(j) << endl;
+			AnalyzeTokens();
+			cout << "  " << token.at(j) << endl;
 			j++;
 		}
 
 	}
 	cout << "Number of Tokens are: " << token.size() - 1;
-
 }
