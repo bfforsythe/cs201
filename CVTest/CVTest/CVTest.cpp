fda1b5ffcb4732bf98c0f7085f03e971346d6c91
Brandon Forsythe <business.brandonforsythe@gmail.com>
2021-04-21 15:58:56 -0800

    Added random color values for foreground/background

--- CVTest/CVTest/CVTest.cpp
+++ CVTest/CVTest/CVTest.cpp
@@ -3,12 +3,15 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
-#include <stdlib.h>
 #include <Windows.h>
 #include <mmsystem.h>
+#include "randomcast.h";
+
 
 
 using namespace cv;
+int blankspace = 225;
+
 
 void main() {
 
@@ -17,16 +20,35 @@ void main() {
     cv::waitKey(2000);
     destroyWindow("img");
 
+    randomGen();
+    int mainColor = castVal.at(0);
+    int mainColor2 = castVal.at(1);
+    int mainColor3 = castVal.at(2);
+
+
+    for (int parse = 0; parse < 3; parse++) {
+        std::cout << castVal.at(parse);
+        std::cout << "\n";
+    }
+
 
     for (int j = 0; j < img.rows; j++)
     {
         for (int i = 0; i < img.cols; i++)
         {
             Vec3b color = img.at<Vec3b>(Point(i, j));
-           
-            color[0] = rand() % 256;
-            color[1] = rand() % 256;
-            color[2] = rand() % 256;
+
+            if (color[1] >= blankspace && color[2] >= blankspace && color[0] >= blankspace) {
+                        color[0] = rand() % 256;
+                        color[1] = rand() % 256;
+                        color[2] = rand() % 256;
+            }
+
+            else {
+                color[0] = mainColor;
+                color[1] = mainColor2;
+                color[2] = mainColor3;
+            }
 
             img.at<Vec3b>(Point(i, j)) = color;
             
@@ -35,4 +57,5 @@ void main() {
     PlaySound(TEXT("C:\\Users\\Forsy\\Downloads\\bonk_AgRFvsD.wav"), NULL, SND_FILENAME);
     cv::imshow("After", img);
     cv::waitKey();
+
 }
